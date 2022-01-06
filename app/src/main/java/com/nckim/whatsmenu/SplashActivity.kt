2e044b13d02a4ba6903f9fb64c2ad176905baefb
nckim <qazqaz1000@hanmail.net>
2022-01-06 23:36:21 +0900

    first commit

---
+++ app/src/main/java/com/nckim/whatsmenu/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.nckim.whatsmenu
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    companion object{
+        private const val DURATION : Long = 3000
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }, DURATION)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
