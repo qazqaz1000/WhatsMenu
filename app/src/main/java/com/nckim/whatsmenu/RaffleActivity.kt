f966905e725a745cbea5ce959bf6eefb5b215551
nckim <nckim@forcs.com>
2022-01-10 17:25:06 +0900

    naver api 검색 test

--- app/src/main/java/com/nckim/whatsmenu/RaffleActivity.kt
+++ app/src/main/java/com/nckim/whatsmenu/RaffleActivity.kt
@@ -18,6 +18,15 @@ import com.nckim.whatsmenu.data.RestaurantData
 import kotlinx.android.synthetic.main.activity_raffle.*
 import java.util.concurrent.TimeUnit
 import com.naver.maps.map.util.FusedLocationSource
+import java.io.*
+import java.lang.RuntimeException
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.net.URLEncoder
+import android.os.StrictMode
+
 
 
 
@@ -49,7 +58,6 @@ class RaffleActivity : AppCompatActivity(), OnMapReadyCallback{
         mapView = findViewById(R.id.mapview)
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
-
     }
 
 
@@ -129,8 +137,83 @@ class RaffleActivity : AppCompatActivity(), OnMapReadyCallback{
             }
 
         }
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+        getRestaurant()
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
     }
 
+    private fun getRestaurant(){
+        val clientId = "ZdiaeLQbLVaYAQODPAUH" //애플리케이션 클라이언트 아이디값"
+
+        val clientSecret = "vY8uGx7Sqm" //애플리케이션 클라이언트 시크릿값"
+
+
+        var text: String? = null
+        text = try {
+            URLEncoder.encode("맛집", "UTF-8")
+        } catch (e: UnsupportedEncodingException) {
+            throw RuntimeException("검색어 인코딩 실패", e)
+        }
+
+        val apiURL =
+            "https://openapi.naver.com/v1/search/blog?query=$text&display=10" // json 결과
+
+        //String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // xml 결과
+
+        //String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // xml 결과
+        val requestHeaders: MutableMap<String, String> = HashMap()
+        requestHeaders["X-Naver-Client-Id"] = clientId
+        requestHeaders["X-Naver-Client-Secret"] = clientSecret
+        val responseBody = get(apiURL, requestHeaders)
+        Log.d("Test", "result : " + responseBody);
+    }
+
+    private operator fun get(apiUrl: String, requestHeaders: Map<String, String>): String? {
+        val con: HttpURLConnection = connect(apiUrl)
+        return try {
+            con.setRequestMethod("GET")
+            for ((key, value) in requestHeaders) {
+                con.setRequestProperty(key, value)
+            }
+            val responseCode: Int = con.getResponseCode()
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                readBody(con.getInputStream())
+            } else { // 에러 발생
+                readBody(con.getErrorStream())
+            }
+        } catch (e: IOException) {
+            throw RuntimeException("API 요청과 응답 실패", e)
+        } finally {
+            con.disconnect()
+        }
+    }
+
+    private fun connect(apiUrl: String): HttpURLConnection {
+        return try {
+            val url = URL(apiUrl)
+            url.openConnection() as HttpURLConnection
+        } catch (e: MalformedURLException) {
+            throw RuntimeException("API URL이 잘못되었습니다. : $apiUrl", e)
+        } catch (e: IOException) {
+            throw RuntimeException("연결이 실패했습니다. : $apiUrl", e)
+        }
+    }
+
+    private fun readBody(body: InputStream): String? {
+        val streamReader = InputStreamReader(body)
+        try {
+            BufferedReader(streamReader).use { lineReader ->
+                val responseBody = StringBuilder()
+                var line: String?
+                while (lineReader.readLine().also { line = it } != null) {
+                    responseBody.append(line)
+                }
+                return responseBody.toString()
+            }
+        } catch (e: IOException) {
+            throw RuntimeException("API 응답을 읽는데 실패했습니다.", e)
+        }
+    }
 }
